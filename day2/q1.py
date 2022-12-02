8a2959712af9493eb105da8a0a5921265e2d69d0
gnpretorius <gnpretorius@gmail.com>
2022-12-02 07:55:34 +0000

    Day 2

---
+++ day2/q1.py
@@ -0,0 +1,39 @@
+def map_value_to_score(val):
+    if val == "X": return 1
+    if val == "Y": return 2
+    if val == "Z": return 3
+
+def play_roshambo(you, opponent):
+    if you == "X" and opponent == "A": return 3 #draw
+    if you == "Y" and opponent == "B": return 3 #draw
+    if you == "Z" and opponent == "C": return 3 #draw
+
+    if you == "X" and opponent == "C": return 6 #win
+    if you == "Y" and opponent == "A": return 6 #win
+    if you == "Z" and opponent == "B": return 6 #win
+
+    if you == "X" and opponent == "B": return 0 #lost
+    if you == "Y" and opponent == "C": return 0 #lost
+    if you == "Z" and opponent == "A": return 0 #lost
+
+
+
+#MAIN
+lines = open('input.txt').read().splitlines() #avoid pending a new line to each line like other methods 
+total_score = 0
+
+for line in lines:
+
+    parts = line.split()
+    
+    game_score = play_roshambo(parts[1], parts[0])
+    hand_score = map_value_to_score(parts[1])
+    total_score += (game_score + hand_score)
+
+
+print(total_score)
+   
+
+
+
+
---
+++ day2/q2.py
@@ -0,0 +1,40 @@
+def calculate_score(outcome, opponent):
+    
+    # X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+    # 1 for Rock, 2 for Paper, and 3 for Scissors
+    # 0 if you lost, 3 if the round was a draw, and 6 if you won
+
+    if outcome == "X": # lose
+        if opponent == "A": return 0 + 3 
+        if opponent == "B": return 0 + 1
+        if opponent == "C": return 0 + 2
+
+    if outcome == "Y": # draw
+        if opponent == "A": return 3 + 1
+        if opponent == "B": return 3 + 2 
+        if opponent == "C": return 3 + 3
+
+    if outcome == "Z": # win
+        if opponent == "A": return 6 + 2
+        if opponent == "B": return 6 + 3
+        if opponent == "C": return 6 + 1
+    
+    
+
+
+
+#MAIN
+lines = open('input.txt').read().splitlines() #avoid pending a new line to each line like other methods 
+total_score = 0
+
+for line in lines:
+    parts = line.split()
+    total_score += calculate_score(parts[1], parts[0])
+
+
+print(total_score)
+   
+
+
+
+
